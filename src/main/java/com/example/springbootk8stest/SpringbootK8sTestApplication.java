b5467e646a75614331cec08bfe1500b4fb775ed8
416643952 <416643952@qq.com>
2020-09-24 14:36:56 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootk8stest/SpringbootK8sTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootk8stest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8sTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sTestApplication.class, args);
+	}
+
+}
